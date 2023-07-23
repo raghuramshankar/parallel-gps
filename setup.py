18934e5cfa8782d6088feeaeeec45a04fecd9973
darkside <raghuram.shankar98@gmail.com>
2023-07-23 09:12:59 +0100

    update requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from setuptools import setup
+import setuptools
 
-with open('requirements.txt') as f:
+with open("requirements2.txt") as f:
     requirements = f.read().splitlines()
 
-setup(
+setuptools.setup(
+    name="pssgp",
     install_requires=requirements,
     author="Adrien Corenflos, Zheng Zhao",
-    version="1.0.0"
+    packages=setuptools.find_packages(
+        where="pssgp",
+        exclude=[
+            "toymodels",
+            "experiments",
+            "experiments.sunspot",
+            "experiments.toy_models",
+            "experiments.co2",
+        ],
+    ),
+    package_dir={
+        "pssgp": "pssgp",
+        "kalman": "pssgp/kalman",
+        "kernels": "pssgp/kernels",
+    },
+    version="1.0.0",
 )
